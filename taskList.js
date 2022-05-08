9038a8977a92fe79f2139695d2741eda8c397093
Carlos Thompson <ext.carlos.thompson@farfetch.com>
2022-05-08 01:57:38 +0100

    working with data attributes and method addEventListener and prevent default behavior

--- taskList.js
+++ taskList.js
@@ -1,7 +1,11 @@
-const newTask = document.querySelector('[data-form-button]')
-
-newTask.addEventListener('click', () => {
+const createTask = (e) => {
+    e.preventDefault()
+    
     const input = document.querySelector('[data-form-input]')
     const value = input.value
     console.log(value)
-})
+    input.value = " "
+}
+
+const newTask = document.querySelector('[data-form-button]')
+    newTask.addEventListener('click', createTask )
