c78a327ef47e117165e82d2769bb12ba5088e588
Carlos Thompson <ext.carlos.thompson@farfetch.com>
2022-05-08 01:47:26 +0100

    Adding command document.querySelector

---
+++ taskList.js
@@ -0,0 +1,7 @@
+const newTask = document.querySelector('[data-form-button]')
+
+newTask.addEventListener('click', () => {
+    const input = document.querySelector('[data-form-input]')
+    const value = input.value
+    console.log(value)
+})
